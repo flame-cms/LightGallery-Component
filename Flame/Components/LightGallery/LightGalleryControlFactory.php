e51dfe7205938c138e9cd5fda3cf7346a4ee45f7
JSifalda <sifalda.jiri@gmail.com>
2012-11-14 19:46:23 +0100

    Add API for LightGallery

--- Flame/Components/LightGallery/LightGalleryControlFactory.php
+++ Flame/Components/LightGallery/LightGalleryControlFactory.php
@@ -13,14 +13,45 @@ namespace Flame\Components\LightGallery;
 class LightGalleryControlFactory extends \Flame\Application\ControlFactory
 {
 
+	/** @var \Flame\Components\LightGallery\LightGalleryControl */
+	private $lightGalleryControl;
+
+	public function __construct()
+	{
+		$this->lightGalleryControl = new LightGalleryControl(array());
+	}
+
+	/**
+	 * @param int $size
+	 */
+	public function setThumbSize($size)
+	{
+		$this->lightGalleryControl->setThumbSize($size);
+	}
+
+	/**
+	 * @param int $size
+	 */
+	public function setMaxSize($size)
+	{
+		$this->lightGalleryControl->setMaxSize($size);
+	}
+
+	/**
+	 * @param int $count
+	 */
+	public  function setImagesPerPageCount($count)
+	{
+		$this->lightGalleryControl->setImagePerPageCount($count);
+	}
 	/**
 	 * @param null $data
 	 * @return LightGalleryControl
 	 */
 	public function create($data = null)
 	{
-		$control = new LightGalleryControl($data);
-		return $control;
+		$this->lightGalleryControl->setImages($data);
+		return $this->lightGalleryControl;
 	}
 
 }
